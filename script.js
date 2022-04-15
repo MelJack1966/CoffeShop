9161d9da329ca5a639523baf755e95ff58af0909
Shaun Graham <shaunkiliangraham@gmail.com>
2022-04-15 12:11:06 -0500

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ var botmanWidget = {
     frameEndpoint: 'chat.html',
     chatServer: 'chat.php',
     introMessage: 'Hello and welcome to Coffee Shop!<br><br>To <b>start over</b> at any time just say "hi" or "hello"',
-    placeholderText: 'Press enter to send',
+    placeholderText: 'Press ENTER to send',
     title: 'Coffee Bot',
     mainColor: '#DF818E',
     bubbleBackground: '#FF2B47',
